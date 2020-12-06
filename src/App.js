d1c8c3f26b48c89b330c2c1531f60485b835b2bf
Julian Betancourt <julianbetancourt@Julians-MacBook-Pro.local>
2020-12-07 00:43:04 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
+import { useState } from 'react'
 import './App.css';
 
+const TODO_STATES = {
+  done: "DONE",
+  notDone: "NOT_DONE",
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [todos, setTodos] = useState([])
+
+  console.log("app")
+  function deleteTodo(todoId) {
+    const newTodos = todos.filter(t => t.id !== todoId)
+    setTodos(newTodos)
+  }
+  
+  function toggleTodoStatus(todoId) {
+    const newTodos = todos.map(t => {
+      if (t.id === todoId) {
+        t.status = t.status === TODO_STATES.done ? TODO_STATES.notDone : TODO_STATES.done
+      }
+      
+      return t
+    }) 
+    setTodos(newTodos)
+  }
+
+  function addTodo(todoName){
+    const id = todos.length + 1
+    let todo = {id, todo: todoName, status: TODO_STATES.notDone}
+    let newTodos = [...todos, todo]
+    setTodos(newTodos)
+  }
+
+  return (<div>
+      <h1>Todo app</h1>
+      <CreateTodo addTodo={addTodo} />
+      <List actvidad="Besar más a Julian"
+       todos={todos}
+       deleteTodo={deleteTodo} toggleStatus={toggleTodoStatus}
+       />
     </div>
   );
 }
 
-export default App;
+function List(props) {
+  console.log("list")
+  return (
+    <ol>
+      {props.todos.map(todo => {
+        return <><li style={{
+          textDecoration: todo.status === TODO_STATES.done ? 'line-through' : 'initial'
+        }}
+        onClick={(evt) => {
+          props.toggleStatus(todo.id)
+        }}>
+          {todo.todo}
+          </li><button onClick={(event) => { props.deleteTodo(todo.id)}}>X</button></>
+      })}
+    </ol>
+  )
+}
+
+function CreateTodo(props) {
+  const [todoText, setTodoText] = useState("")
+
+  return (
+    <><input type="text" onChange={(event) =>{
+      setTodoText(event.target.value)
+    }} value={todoText} />
+    <button onClick={(event) => { 
+      props.addTodo(todoText)
+      setTodoText("")
+    }
+    }>Add</button></>
+  )
+}
+
+export default App
